1bd88b69a19e763975183a55fc2b65aa9411de6c
Hussein Bitambuka <76148389+HusseinBitambuka@users.noreply.github.com>
2022-02-04 16:37:39 -0500

    Add files via upload

---
+++ problem1.java
@@ -0,0 +1,12 @@
+public class problem1 {
+    public static void main(String[] args){
+System.out.println("this program computes the discriminant of 2x^2"
++ "+5x+4=0");
+int a= 2;
+int b=5;
+int c=4;
+int discriminant=(b*b)-(4*a*c);
+System.out.println("The discsriminant for the equation 2x^2+5x+"
++"4=0 is "+ discriminant);
+    }
+}

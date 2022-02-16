caa163e0aea8654fcced9c4944033149db7ea6d7
Hussein Bitambuka <bitambukahoussein@gmail.com>
2022-02-16 14:47:09 -0500

    changes

---
+++ hw/2/loops.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class loops{
+    public static void main(Strings[] args){
+Scanner input= new Scanner(System.in);
+system.out.println("Please enter a string you want to see the number of consecutive character displayed: ");
+double user=input.nextDouble();
+int checker=1;
+while (checker==1){
+    int lenght= int user;
+    String final= String length;
+    System.out.println("The number of digits before the decimal i point in "+ user+ " is "+ final);
+}
+    }
+}
